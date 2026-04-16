3e21136907803db31dba06c2c1cb2f3e5a627433
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-04-16 12:53:06 +0100

    feat(accounts): add surface-aware rh navigation to base layout

--- accounts/templatetags/surface_access.py
+++ accounts/templatetags/surface_access.py
@@ -1,10 +1,15 @@
-# path: accounts/templatetags/surface_access.py
 """Template tags for surface-aware navigation and access checks."""
 
 from django import template
 
-from apps.accounts.constants import GROUP_NAMES, ROLE_ADMIN, ROLE_CUSTOMER, ROLE_MERCHANT, ROLE_OPS
-from apps.accounts.mixins import is_admin_user, user_in_group, user_has_surface_access
+from accounts.constants import (
+    GROUP_NAMES,
+    ROLE_ADMIN,
+    ROLE_CUSTOMER,
+    ROLE_MERCHANT,
+    ROLE_OPS,
+)
+from accounts.mixins import is_admin_user, user_in_group, user_has_surface_access
 
 register = template.Library()
 
@@ -12,6 +17,7 @@ register = template.Library()
 @register.filter
 def has_group(user, role):
     """Return True when the user belongs to the requested role group."""
+
     if role not in GROUP_NAMES:
         return False
     return user_in_group(user, GROUP_NAMES[role])
@@ -20,6 +26,7 @@ def has_group(user, role):
 @register.filter
 def can_access_surface(user, role):
     """Return True when the user may access the requested surface."""
+
     if role not in {ROLE_ADMIN, ROLE_OPS, ROLE_CUSTOMER, ROLE_MERCHANT}:
         return False
     return user_has_surface_access(user, role)
